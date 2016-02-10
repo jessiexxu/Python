c7ff99592946c906e6f2525a858fd8b95b7bf271
Jessie Xu <jessieunc@gmail.com>
2016-02-09 21:29:37 -0800

    Update Codewars Challenges.py

--- Codewars Challenges.py
+++ Codewars Challenges.py
@@ -63,3 +63,12 @@ def goodVsEvil(good, evil):
         return result['evil']
     else:
         return result['tie']
+
+###
+# Challenge 5. Does my number look big in this?
+# A Narcissistic Number is a number which is the sum of its own digits, 
+# each raised to the power of the number of digits.
+# Return true or false depending upon whether the given number is a Narcissistic number.
+
+def narcissistic(value):
+    return sum(int(i)**len(str(value)) for i in str(value)) == value
