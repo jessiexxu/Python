eda2316f6ebd27d76a971b65bddf394b9f97ac09
Jessie Xu <jessieunc@gmail.com>
2016-02-17 21:25:53 -0800

    Update Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -1,1 +1,20 @@
+#################
+## 1. Naive Bayes
+#################
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
 
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+    
+    ### calculate and return the accuracy on the test data
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(labels_test, pred)
+    return accuracy
