4c0ab1abbc7684d0165a382cd8309109bd9274ff
Jessie Xu <jessieunc@gmail.com>
2016-02-28 17:05:29 -0800

    Update Intro to Python.py

--- Intro to Python.py
+++ Intro to Python.py
@@ -148,7 +148,58 @@ def ask():
     print "Thank you, you number squared is: ", num**2
 
 
-###### 4. Battleship Game
+###### 4. Object Oriented Programming
+
+## Problem 1 Fill in the Line class methods to accept coordinate as a pair of 
+## tuples and return the slope and distance of the line.
+import math
+class Line(object):
+    
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return math.sqrt((x2-x1)**2+(y2-y1)**2)
+    
+    def slope(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return (y2-y1)/float(x2-x1)
+
+# EXAMPLE OUTPUT
+
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
+li.distance()
+li.slope()
+
+## Problem 2. Fill in the class
+class Cylinder(object):
+    
+    def __init__(self,height=1,radius=1):
+        self.height = height
+        self.radius = radius
+        
+    def volume(self):
+        return round(math.pi*self.radius**2*self.height,1)
+    
+    def surface_area(self):
+        top = math.pi*self.radius**2
+        return round(2*top + 2*math.pi*self.radius*self.height,1)
+
+# EXAMPLE OUTPUT
+c = Cylinder(2,3)
+c.volume()
+c.surface_area()
+
+
+
+###### 5. Battleship Game
 
 # In this project I will build a simplified, one-player version of the classic board game Battleship! 
 # There will be a single ship hidden in a random location on a 5x5 grid. 
